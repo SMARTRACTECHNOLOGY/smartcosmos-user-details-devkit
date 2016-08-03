6d5fea814ac72e32db848ef75efb03fc46d7965f
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-08-03 18:09:40 +0200

    Re-enable Basic Auth

--- src/main/java/net/smartcosmos/cluster/userdetails/UserDetailsService.java
+++ src/main/java/net/smartcosmos/cluster/userdetails/UserDetailsService.java
@@ -5,14 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import net.smartcosmos.annotation.EnableSmartCosmosEvents;
 import net.smartcosmos.annotation.EnableSmartCosmosExtension;
+import net.smartcosmos.cluster.userdetails.config.ServiceUserAccessSecurityConfiguration;
 
 @EnableSmartCosmosExtension
 @EnableSmartCosmosEvents
+@Import(ServiceUserAccessSecurityConfiguration.class)
 @Slf4j
 public class UserDetailsService extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
 
